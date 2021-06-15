15381a198ca8bf7f9fc153eaf5fdae89510859a5
Rohan Padhye <rohanpadhye@cmu.edu>
2021-06-15 17:28:32 -0400

    TimSort: Repro from top-level fuzz-results corpus to match README

--- src/test/java/sort/TimSortTest.java
+++ src/test/java/sort/TimSortTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RunWith(JQF.class)
 public class TimSortTest {
 
-    @Fuzz(repro="target/fuzz-results/sort.TimSortTest/testTimSort/corpus")
+    @Fuzz(repro="target/fuzz-results/corpus/")
     public void testTimSort(@Size(max=SortTest.MAX_SIZE) List<@InRange(minInt=SortTest.MIN_ELEMENT, maxInt=SortTest.MAX_ELEMENT) Integer> input) {
         SortTest.testSort(new TimSort(), input);
     }
